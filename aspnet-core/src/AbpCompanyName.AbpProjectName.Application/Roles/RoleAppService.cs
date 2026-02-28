e2899381873bc8bb8272fd385bce5f946a30d6ec
Charles Nicholas Kellett <nkellett@deploy.solutions>
2026-02-28 16:47:09 -0500

    Fix description property reference

--- aspnet-core/src/AbpCompanyName.AbpProjectName.Application/Roles/RoleAppService.cs
+++ aspnet-core/src/AbpCompanyName.AbpProjectName.Application/Roles/RoleAppService.cs
@@ -113,7 +113,7 @@ public class RoleAppService : AsyncCrudAppService<Role, RoleDto, System.Guid, Pa
         return (IQueryable<Role>)Repository.GetAllIncluding(x => x.Permissions)
             .WhereIf(!input.Keyword.IsNullOrWhiteSpace(), x => x.Name.Full.Contains(input.Keyword)
             || x.DisplayName.Contains(input.Keyword)
-            || x.Description.Contains(input.Keyword));
+            || x.Description.Full.Contains(input.Keyword));
     }
 
     protected override async Task<Role> GetEntityByIdAsync(System.Guid id)
