98ce65c8c210102b1db3be2456a533b27d18b43b
Charles Nicholas Kellett <nkellett@deploy.solutions>
2026-03-01 09:04:31 -0500

    Remove obsolete modelbuilder code

--- aspnet-core/src/AbpCompanyName.AbpProjectName.EntityFrameworkCore/EntityFrameworkCore/AbpProjectNameDbContext.cs
+++ aspnet-core/src/AbpCompanyName.AbpProjectName.EntityFrameworkCore/EntityFrameworkCore/AbpProjectNameDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Abp.Zero.EntityFrameworkCore;
+﻿using Abp.Localization;
+using Abp.Zero.EntityFrameworkCore;
 using AbpCompanyName.AbpProjectName.Authorization.Roles;
 using AbpCompanyName.AbpProjectName.Authorization.Users;
 using AbpCompanyName.AbpProjectName.MultiTenancy;
@@ -6,6 +7,7 @@ using Deploy.LaunchPad.Core.Metadata;
 using Deploy.LaunchPad.Util.Elements;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System;
 using System.Data;
 using System.Linq;
 
@@ -23,49 +25,7 @@ public class AbpProjectNameDbContext : AbpZeroDbContext<Tenant, Role, User, AbpP
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
-        var entityTypes = modelBuilder.Model.GetEntityTypes().ToList();
 
-        foreach (var entityType in entityTypes)
-        {
-            if (typeof(ILaunchPadMinimalProperties).IsAssignableFrom(entityType.ClrType))
-            {
-                var nameProperty = entityType.ClrType.GetProperty("Name");
-                if (nameProperty == null)
-                {
-                    modelBuilder.Entity(entityType.ClrType).OwnsOne(
-                    typeof(ElementName), "Name",
-                    nameBuilder =>
-                    {
-                        nameBuilder.Property<string>("Short")
-                            .HasColumnName("core_name_short")
-                            .HasMaxLength(50);
-
-                        nameBuilder.Property<string>("Full")
-                            .HasColumnName("core_name_full")
-                            .HasMaxLength(255);
-
-                    }
-                    );
-                }
-                var descriptionProperty = entityType.ClrType.GetProperty("Description");
-                if (descriptionProperty == null)
-                {
-                    modelBuilder.Entity(entityType.ClrType).OwnsOne(
-                    typeof(ElementDescription), "Description",
-                    descriptionBuilder =>
-                    {
-                        descriptionBuilder.Property<string>("Short")
-                            .HasColumnName("core_description_short")
-                            .HasMaxLength(255);
-
-                        descriptionBuilder.Property<string>("Full")
-                            .HasColumnName("core_description_full")
-                        ;
-                    }
-                    );
-                }
-            }
-        }
     }
 
 }
